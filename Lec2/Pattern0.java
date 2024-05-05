47fefbef7e72e679a51934af7d528b8bee172d26
monukumar98 <monu012as@gmail.com>
2024-05-05 19:38:13 +0530

    Pattern

---
+++ Lec2/Pattern0.java
@@ -0,0 +1,18 @@
+package Lec2;
+
+public class Pattern0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int i = 1;
+		int star = n;
+		while (i <= star) {
+			System.out.print("* ");
+
+			i++;
+		}
+
+	}
+
+}
