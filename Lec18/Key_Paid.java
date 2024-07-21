ffedcec0e5a18ed5d5c64fb8113c4f18d873b4ee
monukumar98 <monu012as@gmail.com>
2024-07-21 13:43:03 +0530

    Recursion and BackTracking

--- Lec18/Key_Paid.java
+++ Lec18/Key_Paid.java
@@ -1,10 +1,25 @@
 package Lec18;
 
 public class Key_Paid {
+	static String[] key = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String ques = "23";
+		KeyPaid(ques, "");
 
+	}
+
+	public static void KeyPaid(String ques, String ans) {
+		if (ques.length() == 0) {
+			System.out.println(ans);
+			return;
+		}
+		char ch = ques.charAt(0);// '2'
+		String press = key[ch - '0'];// abc
+		for (int i = 0; i < press.length(); i++) {
+			KeyPaid(ques.substring(1), ans + press.charAt(i));
+		}
 	}
 
 }
